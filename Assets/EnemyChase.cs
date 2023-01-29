c619faccb0d75034cf0cfca9b5b0f464d2492196
David <e.ddemers3@etu.cvm.qc.ca>
2023-01-29 00:55:45 -0500

    enemy chase player

---
+++ Assets/EnemyChase.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    public float distance;
+    public LayerMask layerMask;
+    private Vector2 direction;
+    private bool chasing = false;
+    private Rigidbody2D rb;
+    private float speed = 3;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (chasing)
+        {
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 visionDirection = player.transform.position - transform.position;
+        visionDirection.Normalize();
+
+        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, visionDirection, distance,layerMask);
+
+        if (raycastHit.collider != null)
+        {
+            chasing = false;
+            if (raycastHit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                    direction = visionDirection;
+                    chasing = true;
+            }
+        }
+        else
+        {
+            chasing = false;
+        }
+    }
+}
