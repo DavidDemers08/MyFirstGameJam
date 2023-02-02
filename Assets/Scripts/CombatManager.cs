59a433a14ed93ae4e011a75d6994bbb768bc90c2
David Demers <e.ddemers3@etu.cvm.qc.ca>
2023-02-01 22:01:55 -0500

    correction

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -1,19 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CombatManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform playerPosition;
     private GameObject player;
+    public Slider slider;
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+
         
         if (player == null) return;
         player.transform.GetChild(0).gameObject.SetActive(false);
         player.transform.position = playerPosition.position;
+
+        InitHealth();
+    }
+
+    private void InitHealth()
+    {
+        slider.value = MainManager.Instance.currentHp / MainManager.Instance.maxHp;
     }
 
     // Update is called once per frame
